8182a3bc77f85f25768c6bd7405959ae96ee9412
apugithub <apugithub@users.noreply.github.com>
2015-07-08 01:40:14 +0530

    Update finding the largest number.py

--- Week-5/finding the largest number.py
+++ Week-5/finding the largest number.py
@@ -1,1 +1,12 @@
+### Findiding the largest number
+largest=None
+for value in [3,50,1,78,5]:
+  if largest is None:
+    largest=value
+  elif value>largest:
+    largest=value
+    
+print (" Largest is: "), largest    
+    
+    
 

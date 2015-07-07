7e36b8252d058f91ffb1ddd516db3cccc03b4181
apugithub <apugithub@users.noreply.github.com>
2015-07-08 01:38:00 +0530

    Update Finding the smallest number.py

--- Week-5/Finding the smallest number.py
+++ Week-5/Finding the smallest number.py
@@ -1,1 +1,11 @@
-
+### Findiding the smallest number
+smallest=None
+for value in [3,50,1,78,5]:
+  if smallest is None:
+    smallest=value
+  elif value<smallest:
+    smallest=value
+    
+print (" Smallest is: "), smallest    
+    
+    
